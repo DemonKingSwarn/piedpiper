661c50b8955b4f86ec740ff3d99508fb0e865d81
DemonKingSwarn <rockingswarn@gmail.com>
2023-07-10 19:57:29 +0530

    You Like To Fuck My Code?

--- app.py
+++ app.py
@@ -1,16 +1,43 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
 from PIL import Image
+import time
+import threading
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/compressed_images'
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
+app.config['TEMP_FILE_EXPIRATION'] = 3600  # 1 hour in seconds
 
+
+"""
 def compress_image(image):
    compressed_image = image.copy()
    compressed_image.save("compressed.jpg", optimize=True, quality=50)
 
    return compressed_image
+"""
+
+def compress_image(image):
+    compressed_image = image.copy()
+
+    # Get the pixel values of the image
+    pixel_values = list(image.getdata())
+
+    # Convert pixel values to integers
+    flattened_pixels = [p for pixel in pixel_values for p in pixel]
+
+    # Convert integers to bytes
+    byte_values = bytes(flattened_pixels)
+
+    # Create a new PIL image from the byte values
+    compressed_image.frombytes(byte_values)
+
+    return compressed_image
+
+def delete_file_after_timeout(filepath, timeout):
+    time.sleep(timeout)
+    os.remove(filepath)
 
 @app.route('/')
 def upload_form():
@@ -45,6 +72,9 @@ def compress():
 
         # Delete the temporary file
         os.remove(temp_filepath)
+         # Start a thread to delete the file after the timeout
+        timeout_thread = threading.Thread(target=delete_file_after_timeout, args=(compressed_filepath, app.config['TEMP_FILE_EXPIRATION']))
+        timeout_thread.start()
 
         # Redirect to the display page with the compressed image
         return redirect(url_for('display', filename=compressed_filename))
@@ -58,5 +88,6 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=3000)
+
 
